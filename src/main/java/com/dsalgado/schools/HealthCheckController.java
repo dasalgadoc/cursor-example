37066212fdeff5cbf2aee747656b6ee505e64f1c
diesalgado_meli <diego.salgado@mercadolibre.com.co>
2025-05-24 13:16:18 -0500

    boilerplate from spring initializer

---
+++ src/main/java/com/dsalgado/schools/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.dsalgado.schools;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong from schools application";
+    }
+}
---
+++ src/test/java/com/dsalgado/schools/SchoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsalgado.schools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
